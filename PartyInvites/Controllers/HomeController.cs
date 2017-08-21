7520c4483beca18ce2fe1f508515d4d588c8db06
stipanovs <sergiu.stipanov@amdaris.com>
2017-08-21 13:31:31 +0300

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index() //RedirectResult, HttpUnauthorizedResult,
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Start = "Start";
+            ViewBag.Hello = hour < 12 ? "Good morning" : "Good Afternoon";
+
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+              
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
+        }
+    }
+}
